063a3502ee0577eeccb90c163e0d346a12e7b3c2
jerrynabango <jerrynabango9@gmail.com>
2023-03-16 14:06:12 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point of any program
+ *
+ * Return: Indicates success in execution of any program
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return(0);
+}
