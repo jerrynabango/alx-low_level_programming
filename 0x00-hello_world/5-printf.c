8c444c2d96896f22c61d6f8fd2dc835034d67a51
jerrynabango <jerrynabango9@gmail.com>
2023-03-16 14:31:18 -0700

    print formatted data

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Start of any program
+ *
+ * return: Indicates success execution of a program
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
