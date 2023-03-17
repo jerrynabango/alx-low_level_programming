d14d3dfe03a9c6f80f3de8272966827110c9b00f
jerrynabango <jerrynabango9@gmail.com>
2023-03-16 19:54:32 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry
- * return: 1
+ * main - Starting point of the program
+ *
+ * Return: Indicates successful execution of the program
  */
+
+
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
