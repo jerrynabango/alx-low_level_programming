196786cd2d726ce384c78624936af5b1469daf52
jerrynabango <jerrynabango9@gmail.com>
2023-03-21 13:09:44 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Indicates the start of the program
+ *
+ * print_alphabet - Writes the lowercase alphabet
+ */
+
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a' ; j <= 'z' ; j++)
+		_putchar(j);
+	_putchar('\n');
+}
