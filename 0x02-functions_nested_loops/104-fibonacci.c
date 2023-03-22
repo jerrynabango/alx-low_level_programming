143d4480a077896b243d61f28a97dadfa699e604
jerrynabango <jerrynabango9@gmail.com>
2023-03-21 19:59:27 -0700

    100 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,45 +7,45 @@
 
 int main(void)
 {
-		unsigned long int i;
-		unsigned long int u = 1;
-		unsigned long int a = 2;
-		unsigned long int l = 1000000000;
-		unsigned long int u1;
-		unsigned long int u2;
-		unsigned long int a1;
-		unsigned long int a2;
+	int c, boolean, boolean2;
+	long int num1, num2, fn, fn2, num11, num22;
 
-
-		printf("%lu", u);
-
-
-		for (i = 1; i < 91; i++)
+	num1 = 1;
+	num2 = 2;
+	boolean =  boolean2 = 1;
+	printf("%ld, %ld", num1, num2);
+	for (c = 0; c < 96; c++)
+	{
+		if (boolean)
 		{
-			printf(", %lu", a);
-			a += u;
-			u = a - u;
+			fn = num1 + num2;
+			printf(", %ld", fn);
+			num1 = num2;
+			num2 = fn;
 		}
-
-
-		u = (u / l);
-		u2 = (u % l);
-		a1 = (a / l);
-		a2 = (a % l);
-
-
-		for (i = 92; i < 99; ++i)
+		else
 		{
-			printf(", %lu", a1 + (a2 / l));
-			printf("%lu", a2 % l);
-			a1 = a1 + u1;
-			u1 = a1 - u1;
-			a2 = a2 + u2;
-			u2 = a2 - u2;
+			if (boolean2)
+			{
+				num11 = num1 % 1000000000;
+				num22 = num2 % 1000000000;
+				num1 = num1 / 1000000000;
+				num2 = num2 / 1000000000;
+				boolean2 = 0;
+			}
+			fn2 = (num11 + num22);
+			fn = num1 + num2 + (fn2 / 1000000000);
+			printf(", %ld", fn);
+			printf("%ld", fn2 % 1000000000);
+			num1 = num2;
+			num11 = num22;
+			num2 = fn;
+			num22 = (fn2 % 1000000000);
 		}
-		printf("\n");
-		return (0);
+		if (((num1 + num2) < 0) && boolean == 1)
+			boolean = 0;
+	}
+	printf("\n");
+	return (0);
 }
-
-
 
