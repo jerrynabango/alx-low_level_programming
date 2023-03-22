8fc2fabd4b12afe17dac1044092ae88c212c3d4f
jerrynabango <jerrynabango9@gmail.com>
2023-03-21 20:10:58 -0700

    100 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,43 +7,43 @@
 
 int main(void)
 {
-	int c, boolean, boolean2;
-	long int num1, num2, fn, fn2, num11, num22;
+	int a, grade, grade2;
+	long int num1, num2, fib, fib2, num11, num22;
 
 	num1 = 1;
 	num2 = 2;
-	boolean =  boolean2 = 1;
+	grade =  grade2 = 1;
 	printf("%ld, %ld", num1, num2);
-	for (c = 0; c < 96; c++)
+	for (a = 0; a < 96; a++)
 	{
 		if (boolean)
 		{
-			fn = num1 + num2;
+			fib = num1 + num2;
 			printf(", %ld", fn);
 			num1 = num2;
-			num2 = fn;
+			num2 = fib;
 		}
 		else
 		{
-			if (boolean2)
+			if (grade2)
 			{
 				num11 = num1 % 1000000000;
 				num22 = num2 % 1000000000;
 				num1 = num1 / 1000000000;
 				num2 = num2 / 1000000000;
-				boolean2 = 0;
+				grade2 = 0;
 			}
-			fn2 = (num11 + num22);
-			fn = num1 + num2 + (fn2 / 1000000000);
-			printf(", %ld", fn);
-			printf("%ld", fn2 % 1000000000);
+			fib2 = (num11 + num22);
+			fib = num1 + num2 + (fib2 / 1000000000);
+			printf(", %ld", fib);
+			printf("%ld", fib2 % 1000000000);
 			num1 = num2;
 			num11 = num22;
-			num2 = fn;
-			num22 = (fn2 % 1000000000);
+			num2 = fib;
+			num22 = (fib2 % 1000000000);
 		}
-		if (((num1 + num2) < 0) && boolean == 1)
-			boolean = 0;
+		if (((num1 + num2) < 0) && grade == 1)
+			grade = 0;
 	}
 	printf("\n");
 	return (0);
