929815d4e0ac1a172bcd83b0619d3f263a1531e8
jerrynabango <jerrynabango9@gmail.com>
2023-03-23 14:53:11 -0700

    _putchar

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers - checks for checks for a digit (0 through 9).
+ * print_numbers - Checks for numbersbtn 0-9
  *
- * Return: Always 0.
+ * Return: Indicates successful execution
  */
 void print_numbers(void)
 {
