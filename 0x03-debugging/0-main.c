f6b7ee8fed3fb6d4503727e8519542c224de358b
jerrynabango <jerrynabango9@gmail.com>
2023-03-23 06:16:34 -0700

    multiple lines

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * main - Start of the program
- *
- * Return: Indicates successful execution
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 	return (0);
