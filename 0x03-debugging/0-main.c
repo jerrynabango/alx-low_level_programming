badfc9110aacf833822cd52bdb95b32efa6881f4
jerrynabango <jerrynabango9@gmail.com>
2023-03-23 05:14:31 -0700

    positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Start of the program
+ *
+ * Return: Indicates successful execution
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
