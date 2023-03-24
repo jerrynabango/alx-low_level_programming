7dbae383a72e593fc1cfa2165df921f1dd954387
jerrynabango <jerrynabango9@gmail.com>
2023-03-23 17:35:35 -0700

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Displays the prime factors
+ *
+ * Return: Indicates successful execution
+ */
+int main(void)
+{
+	unsigned long int a, b = 612852475143;
+
+	for (a = 3 ; a < 800000 ; a = a + 2)
+	{
+		while ((b % a == 0) && (b != a))
+			b = b / a;
+	}
+	printf("%lu\b", b);
+	return (0);
+}
+
