f2a363a41b9e3f51e13604b895e34fcdf0774e0f
jerrynabango <jerrynabango9@gmail.com>
2023-03-23 17:55:56 -0700

    prime factors & composite numbers

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	unsigned long int i, n = 612852475143;
 
-	for (i = 3; a < 782849; i = i + 2)
+	for (i = 3; i < 782849; i = i + 2)
 	{
 		while ((n % i == 0) && (n != i))
 			n = n / i;
