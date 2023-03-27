c9f926059e0597284dd926f7b80a174f80f38356
jerrynabango <jerrynabango9@gmail.com>
2023-03-26 22:47:01 -0700

    change pointer to integer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that takes a pointer to an int
+ *
+ * @n: The number to be changed
+ *
+ * Return: Empty
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
