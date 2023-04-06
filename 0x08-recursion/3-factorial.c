344054a6671f60ed8953d2173df93d7df38960c7
jerrynabango <jerrynabango9@gmail.com>
2023-04-05 20:07:59 -0700

    returns value raised to power

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - A function that returns the factorial of a given number
+ *
+ * @n: Indicates the factorial number
+ *
+ * return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
