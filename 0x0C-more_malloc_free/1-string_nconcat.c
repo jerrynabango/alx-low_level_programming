0105fb030a369e85ca3fa1de9ce572ab58e80446
jerrynabango <jerrynabango9@gmail.com>
2023-04-13 10:05:16 -0700

    concatenate strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *concat;
 	unsigned int string1, string2, string3;
 
-	if (s1 == NULL)_
+	if (s1 == NULL)
 	{
 		string1 = 0;
 	}
@@ -25,4 +25,32 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		for (string1 = 0; s1[string1]; ++string1)
 			;
 	}
-
+	if (s2 == NULL)
+	{
+		string2 = 0;
+	}
+	else
+	{
+		for (string2 = 0; s2[string2]; ++string2)
+			;
+	}
+	if (string2 > n)
+	{
+		string2 = n;
+	}
+	concat = malloc(sizeof(char) * (string1 + string2 + 1));
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+	for (string3 = 0 ; string3 < string1 ; string3++)
+	{
+		concat[string3] = s1[string3];
+	}
+	for (string3 = 0 ; string3 < string2 ; string3++)
+	{
+		concat[string3 + string1] = s2[string3];
+	}
+	concat[string1 + string2] = '\0';
+	return (concat);
+}
