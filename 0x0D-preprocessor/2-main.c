d2fd1e0e120f690265f8de55e86cb06e2ecd0984
jerrynabango <jerrynabango9@gmail.com>
2023-04-17 09:15:26 -0700

    macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the name of the file it was compiled
+ *
+ * Return: Displays the name of the file compiled
+ */
+int main(void)
+{
+	printf("%s\n",__FILE__);
+
+	return (0);
+}
