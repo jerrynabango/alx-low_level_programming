9d61fb584cd1e429a4bdf75f767a3a57487baa85
jerrynabango <jerrynabango9@gmail.com>
2023-04-27 13:00:01 -0700

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -13,10 +13,9 @@ void free_list(list_t *head)
 	if (head)
 	{
 		free_list(head->next);
+
+		if (head->str)
+			free(head->str);
+		free(head);
 	}
-	if (head->str)
-	{
-		free(head->str);
-	}
-	free(head);
 }
