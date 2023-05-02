3449a574c6006086704658fce0297276a699f051
jerrynabango <jerrynabango9@gmail.com>
2023-05-02 07:09:39 -0700

    safe version

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,6 +2,54 @@
 #include "lists.h"
 
 /**
+ * listint_print - A function to safe list
+ *
+ * @safe1: Indicates the pointer
+ *
+ * Return: Displays the list
+ */
+size_t listint_print(const listint_t *safe1)
+{
+	size_t temp;
+	const listint_t *node, *list;
+
+	temp = 1;
+
+	if (safe1 == NULL || safe1->next == NULL)
+		return (0);
+
+	node = safe1->next;
+	list = (safe1->next)->next;
+
+	while (list)
+	{
+		if (node == list)
+		{
+			node = safe1;
+			while (node != list)
+			{
+				temp++;
+				node = node->next;
+				list = list->next;
+			}
+			node = node->next;
+
+			while (node != list)
+			{
+				temp++;
+				node = node->next;
+			}
+
+			return (temp);
+		}
+		node = node->next;
+		list = (list->next)->next;
+	}
+
+	return (0);
+}
+
+/**
  * print_listint_safe - A function that prints list
  *
  * @head: Indciates the pointer to the listint_t
