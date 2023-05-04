c8e4f3121532c23b14f0f27b7193bfa195231409
jerrynabango <jerrynabango9@gmail.com>
2023-05-04 12:48:00 -0700

    check endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,13 +3,13 @@
 /**
  * get_endianness - A function that checks the endianness
  *
- * return: Displays the endianness
+ * Return: Displays the endianness
  */
 int get_endianness(void)
 {
 	char *end;
 	int check;
-	
+
 	end = (char *)&check;
 	check = 1;
 
