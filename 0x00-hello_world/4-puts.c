8d5623eba936582421a8611871dd17653513cb45
Jerry Nabango <jerrynabango9@gmail.com>
2023-05-09 10:05:15 +0300

    Hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point of any program
+ *
+ * Return: Indicates success in execution of any program
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Startng point of any program
+ *
+ * Return: Indicates success execution of a program
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
