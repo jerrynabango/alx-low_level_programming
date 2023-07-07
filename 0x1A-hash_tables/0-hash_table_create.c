7411c0b81c117fdee8ca162a909d17be8d715e5e
jerrynabango <jerrynabango9@gmail.com>
2023-07-08 01:08:32 +0300

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,20 +12,21 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *key;
 	unsigned long int value;
 
+	value = 0;
+
 	key = malloc(sizeof(hash_table_t));
 	if (key == NULL)
-	{
 		return (NULL);
-	}
+
 	key->size = size;
 	key->array = malloc(sizeof(hash_node_t *) * size);
 	if (key->array == NULL)
-	{
 		return (NULL);
-	}
-	for (value = 0; value < size; value++)
+	while (value < size)
 	{
 		key->array[value] = NULL;
 	}
+		value++;
+
 	return (key);
 }
